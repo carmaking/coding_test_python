d44fce9cd2a8d1ac4f99788237d2f516f234285b
carmaking <79752918+carmaking@users.noreply.github.com>
2021-03-30 15:24:18 +0900

    Create 11047.py

---
+++ BOJ/11047.py
@@ -0,0 +1,19 @@
+N, K = map(int, input().split())
+
+coins = []
+for i in range(N):
+    coins.append(int(input()))
+
+coins.sort(reverse = True)
+
+count = 0
+        
+for coin in coins:
+    if coin <= K:
+        count += K // coin
+        K %= coin
+    
+    if K == 0:
+        break
+
+print(count)
