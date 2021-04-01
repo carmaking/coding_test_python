063cfa4333d38ed3470bc10ba2d8e39a39c8eb73
carmaking <79752918+carmaking@users.noreply.github.com>
2021-04-01 14:03:14 +0900

    Create 1260.py

---
+++ BOJ/1260.py
@@ -0,0 +1,30 @@
+N, M, V = map(int, input().split())
+
+data = [[0] * (N+1) for _ in range(N+1)]
+visited = [False] * (N+1)
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    data[a][b] = 1
+    data[b][a] = 1
+
+def dfs(v):
+    visited[v] = True
+    print(v,end=' ')
+    for i in range(1, N+1):
+        if not visited[i] and data[v][i] == 1:
+            dfs(i)
+    
+def bfs(v):
+    queue = [v]
+    visited[v] = False
+    while queue:
+        v = queue.pop(0)
+        print(v,end=' ')
+        for i in range(1,N+1):
+            if visited[i] and data[v][i] == 1:
+                queue.append(i)
+                visited[i] = False
+dfs(V)
+print()
+bfs(V)
